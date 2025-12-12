a739a586c16959ef9c6f8864184a52560c93ea0b
donmartins <doncodesacademy@gmail.com>
2025-12-12 18:21:36 +0100

    color change

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -38,7 +38,7 @@ const Hero = () => {
               ))}
           </div>
 
-          <p className="bold-16 lg:bold-20 text-orange-600">
+          <p className="bold-16 lg:bold-20 text-black-600">
             198k
             <span className="regular-16 lg:regular-20 ml-1">Beta Reviews</span>
           </p>
