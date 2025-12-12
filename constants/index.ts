13efdc102622a8d3bfb0346313a38141a439f4dc
donmartins <doncodesacademy@gmail.com>
2025-12-12 18:20:08 +0100

    text adjustment

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
 // NAVIGATION
 export const NAV_LINKS = [
   { href: '/', key: 'home', label: 'Home' },
-  { href: '/', key: 'how_TravelLink_work', label: 'How TravelLink Work?' },
+  { href: '/', key: 'how_TravelLink_work', label: 'How it Works?' },
   { href: '/', key: 'services', label: 'Services' },
   { href: '/', key: 'pricing ', label: 'Pricing ' },
   { href: '/', key: 'contact_us', label: 'Contact Us' },
