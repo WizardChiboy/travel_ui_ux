008c7d32b73aed42e3663ac3cf5e1340a7c8bb95
donmartins <doncodesacademy@gmail.com>
2025-12-12 18:17:14 +0100

    Travel ui/ux

---
+++ components/Guide.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import React from "react";
+
+const Guide = () => {
+  return (
+    <section className="flexCenter flex-col">
+      <div className="padding-container max-container w-full pb-24">
+        <Image src="/camp.svg" alt="camp" width={50} height={50} />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-black-500">
+          We dey for you
+        </p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[390px] text-black-700">
+            We Go Show You Easy Road
+          </h2>
+          <p className="regular-16 text-gray-500 xl:max-w-[520px]">
+            With NaijaTravel app, you no go ever lost for road again. Even if
+            network no dey, our offline map go still guide you. Carry your padi,
+            family and enjoy beta adventure for bush, valley and reach the top!
+          </p>
+        </div>
+      </div>
+
+      <div className="flexCenter max-container relative w-full">
+        <Image
+          src="/boat.png"
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center 2xl:rounded-5xl"
+        />
+
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 gap-3 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image
+            src="/meter.svg"
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-400">Destination</p>
+                <p className="bold-16 text-orange-600">35 min</p>
+              </div>
+              <p className="bold-20 mt-2">Obudu Cattle Ranch</p>
+            </div>
+
+            <div className="flex w-full flex-col">
+              <p className="regular-16 text-gray-400">Start track</p>
+              <h4 className="bold-20 mt-2 whitespace-nowrap">Lekki Phase 1</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Guide;
